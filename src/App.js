5a34723ebe9e19fc431246a85610704567bd7541
peachy-manasis <peachy.manasis@crmworks.asia>
2017-04-01 17:59:32 +0800

    master
    - added pagination to pokeapi request

--- src/App.js
+++ src/App.js
@@ -3,35 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 import PokeList from './components/PokeList';
-import { Col } from 'react-bootstrap/lib/';
+import { Col, Pagination } from 'react-bootstrap/lib/';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      pokemon: []
+      pokemon: [],
+      activePage: 0,
+      limit: 50,
+      offset: 0,
+      totalPages: 0
     };
-
     this.loadPokemon = this.loadPokemon.bind(this);
+    this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
   }
+
   loadPokemon(url) {
     fetch(url)
     .then(response => {
       return response.json();
-    }).then(json => {
-      console.log(json)
+    })
+    .then(json => {
+      console.log(json);
+      let pages = Math.round(json.count / this.state.limit);
+
       this.setState({
-        pokemon: json.results
+        pokemon: json.results,
+        totalPages: pages,
+        count: json.count
       })
       console.log(this.state)
-    }).catch(error => {
+    })
+    .catch(error => {
       console.log(error)
     })
   }
 
   componentWillMount() {
-    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${this.state.offset}`);
+  }
+
+  handlePaginationSelect(selectedPage) {
+    console.log(selectedPage);
+    let offset = this.state.limit * selectedPage;
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${offset}`);
   }
 
   render() {
@@ -43,6 +60,16 @@ class App extends Component {
         </div>
         <Col sm={8} md={10} smOffset={2} mdOffset={1} >
           <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
+
+        <Col sm={12} >
+          <Pagination 
+            bsSize='small' 
+            items={this.state.totalPages} 
+            activePage={this.state.activePage} 
+            onSelect={this.handlePaginationSelect}
+
+            />
         </Col>
       </div>
     );
