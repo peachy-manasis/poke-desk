a9d8980713177f4d0012e51c5d7b50d9ffca0482
peachy-manasis <peachy.manasis@crmworks.asia>
2017-07-05 13:34:07 +0800

    client build for the front-end app (react)

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 app.set("port", process.env.PORT || 3001);
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-};
+    app.use(express.static('client/build'));
+}
+
 
 app.listen(app.get("port"), () => {
     console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
