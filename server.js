cb7eb0f6f9d250db6f4fe1261640132586ec659b
peachy-manasis <peachy.manasis@crmworks.asia>
2017-07-05 13:38:58 +0800

    playing around server.js configuration to what works with heroku

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
+app.set('port', process.env.PORT || 3001)
 
-app.use(cors())
-
-app.set("port", process.env.PORT || 3001);
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
 
-
 app.listen(app.get("port"), () => {
-    console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
-});
+  console.log(`Server running at ${app.get("port")}`);});
